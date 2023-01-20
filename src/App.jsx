7374b8d710fbc106189119c1059444c8ff5d7412
calebgl <caleb.csgl@gmail.com>
2023-01-20 16:05:45 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { Icon } from "@iconify/react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
