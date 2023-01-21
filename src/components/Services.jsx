4e5aea5b8d600d8a2a34ade3e08eec2d1cb05e05
calebgl <caleb.csgl@gmail.com>
2023-01-20 23:40:20 -0600

    refactor: implement dynamic services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,3 +1,5 @@
+import { Icon } from "@iconify/react";
+import { services } from "../data";
 import Title from "./Title";
 
 function Services() {
@@ -6,44 +8,17 @@ function Services() {
       <Title title="our" subtitle="services" />
 
       <div className="section-center services-center">
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-wallet fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">saving money</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-tree fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">endless hiking</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-socks fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">amazing comfort</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
+        {services.map((service) => (
+          <article key={service.icon} className="service">
+            <span className="service-icon">
+              <Icon icon={service.icon} />
+            </span>
+            <div className="service-info">
+              <h4 className="service-title">{service.title}</h4>
+              <p className="service-text">{service.text}</p>
+            </div>
+          </article>
+        ))}
       </div>
     </section>
   );
--- src/data.js
+++ src/data.js
@@ -8,4 +8,22 @@ export const pageLinks = [
 export const socialLinks = [
   { href: "https://www.twitter.com", icon: "mdi:github" },
   { href: "https://www.twitter.com", icon: "mdi:linkedin" },
-]
+];
+
+export const services = [
+  {
+    icon: "material-symbols:attach-money-rounded",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    icon: "material-symbols:hiking-rounded",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    icon: "mdi:sofa",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
